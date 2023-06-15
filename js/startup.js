2dadba39352334ee6493b7ea93401c44577ad297
Smodoopa <smodoopa@gmail.com>
2023-06-15 00:54:30 -0400

    Clean up main.js

--- js/startup.js
+++ js/startup.js
@@ -1,3 +1,20 @@
 function startUp() {
-    console.log("SteamEnhance activated.")
+    $.fn.insertAt = function (index, element) {
+        var lastIndex = this.children().size();
+        if (index < 0) {
+            index = Math.max(0, lastIndex + 1 + index);
+        }
+        this.append(element);
+        if (index < lastIndex) {
+            this.children().eq(index).before(this.children().last());
+        }
+        return this;
+    }
+    
+    $('.btn-watchnow').click();
+    console.log("SteamEnhance activated.");
+
+
+    window.scrollTo(0, 0);
+    $('.jw-icon-fullscreen').click();
 }
--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // @description  In loving memory of Soap.
 // @icon         https://www.google.com/s2/favicons?domain=google.com
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @require      E:\Code\TamperMonkey\fMovies\js\startup.js
+// @require      https://raw.githubusercontent.com/Smodoopa/SteamingEnhance/main/main.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/Smodoopa/SteamingEnhance/main/style.css
@@ -20,17 +20,4 @@
     setTimeout(() => {
         startUp();
     }, 5000);
-    
 })();
-
-
-
-function startUp() {
-    console.log("SteamEnhance activated.");
-    $('.btn-watchnow').click();
-
-    setTimeout(() => {
-        window.scrollTo(0, 0);
-        $('.jw-icon-fullscreen').click();
-    }, 3000);
-}
