aa7e72e93a8bfd87b26e07764bf10487464be3ea
Smodoopa <smodoopa@gmail.com>
2023-06-15 23:27:46 -0400

    Styling 12

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // @description  In loving memory of Soap.
 // @icon         https://www.google.com/s2/favicons?domain=google.com
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @require      https://raw.githubusercontent.com/Smodoopa/SteamingEnhance/main/main.js
+// @require      https://raw.githubusercontent.com/Smodoopa/SteamingEnhance/main/js/startup.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/Smodoopa/SteamingEnhance/main/style.css
