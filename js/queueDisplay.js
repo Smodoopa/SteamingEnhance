93c42836208f7a500de8b97e896c1021980bf358
Smodoopa <smodoopa@gmail.com>
2023-06-26 22:54:18 -0400

    Queue Init 2

--- js/queueDisplay.js
+++ js/queueDisplay.js
@@ -1,39 +1,35 @@
+const queueHeaderBtn = '<div class="seQueue"><button data-toggle="dropdown" data-placeholder="false"><i class="bi bi-list-ul"></i> </button></div>';
+
 const queueInit = () => {
     loadQueue();
-
-    
 }
-
-
 
 const loadQueue = () => {
     if (!localStorage.getItem('myQueue')) localStorage.setItem('myQueue', '[]');
 }
 
-
-
-
-
-const addToQueue = url => {
-    var myQueue = JSON.parse(localStorage.getItem('myQueue'));
-
-    myQueue.push([text, url]);
-
-    localStorage.setItem("myQueue", JSON.stringify(myQueue));
-    triggerNoticiation(`${text} successfully added to your queue!`);
+const loadQueueModal = () => {
+        //Add Queue Modal to Site Nav Header
+        $('body').prepend('<div class="queue-modal"><div class="queue-modal-content"><div class="queue-details"><div class="server-header"><h3 class="header-text">My Queue</h3><div id="close-btn" class="server-header-btn"><i class="bi bi-x-lg"></i></div></div><table class="queue-table"><tbody><tr class="queue-table-headers"><th>Order</th><th>Name</th></tr></tbody></table><div class="queue-button-panel"><div id="btnFav" class="btn btn-primary"><i class="fa fa-star" aria-hidden="true"></i></div><div id="btnQueueClear" class="btn btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></div><div id="btnShuffleQueue" class="btn btn-primary"><i class="fa fa-random" aria-hidden="true"></i></div><input class="quickAddInput" placeholder="Quick Add"><div class="btn btn-primary quickAddSubmit"><i class="fa fa-search" aria-hidden="true"></i></div></div></div></div></div>');
+    
+        $('.seQueue').click(() => displayQueueModal());
 }
 
+const addToQueue = url => {
+    //var myQueue = JSON.parse(localStorage.getItem('myQueue'));
 
+    //myQueue.push([text, url]);
 
-const loadQueueModal = () => {
-    loadQueueItems();
+    //localStorage.setItem("myQueue", JSON.stringify(myQueue));
+    //triggerNoticiation(`${text} successfully added to your queue!`);
+}
 
-    $('#btnFav').click(e => { loadFavoritesList() });
-
+const displayQueueModal = () => {
+    //loadQueueItems();
+    //$('#btnFav').click(e => { loadFavoritesList() });
     $('.queue-modal').css('display', 'flex');
     $('body').toggleClass('noscroll');
 }
-
 
 const closeQueueModal = () => {
     $('.queue-modal').hide();
--- js/startup.js
+++ js/startup.js
@@ -1,3 +1,4 @@
+
 function startUp() {
     $.fn.insertAt = function (index, element) {
         var lastIndex = this.children().size();
@@ -14,9 +15,54 @@ function startUp() {
     $('.btn-watchnow').click();
     console.log("SteamEnhance activated.");
 
+    queueInit();
 
     window.scrollTo(0, 0);
     $('.jw-icon-fullscreen').click();
+}
 
-    $('body').prepend('<div class="queue-modal"><div class="queue-modal-content"><div class="queue-details"><div class="server-header"><h3 class="header-text">My Queue</h3><div id="close-btn" class="server-header-btn"><i class="fa fa-times" aria-hidden="true"></i></div></div><table class="queue-table"><tbody><tr class="queue-table-headers"><th>Order</th><th>Name</th></tr></tbody></table><div class="queue-button-panel"><div id="btnFav" class="btn btn-primary"><i class="fa fa-star" aria-hidden="true"></i></div><div id="btnQueueClear" class="btn btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></div><div id="btnShuffleQueue" class="btn btn-primary"><i class="fa fa-random" aria-hidden="true"></i></div><input class="quickAddInput" placeholder="Quick Add"><div class="btn btn-primary quickAddSubmit"><i class="fa fa-search" aria-hidden="true"></i></div></div></div></div></div>');
+
+
+
+/*-----------------------------------------------------------------*/
+/*-----------------------------QUEUE-------------------------------*/
+/*-----------------------------------------------------------------*/
+
+const queueHeaderBtn = '<div class="seQueue"><button data-toggle="dropdown" data-placeholder="false"><i class="bi bi-list-ul"></i> </button></div>';
+
+const queueInit = () => {
+    loadQueue();
+}
+
+const loadQueue = () => {
+    if (!localStorage.getItem('myQueue')) localStorage.setItem('myQueue', '[]');
+}
+
+const loadQueueModal = () => {
+        //Add Queue Modal to Site Nav Header
+        $('body').prepend('<div class="queue-modal"><div class="queue-modal-content"><div class="queue-details"><div class="server-header"><h3 class="header-text">My Queue</h3><div id="close-btn" class="server-header-btn"><i class="bi bi-x-lg"></i></div></div><table class="queue-table"><tbody><tr class="queue-table-headers"><th>Order</th><th>Name</th></tr></tbody></table><div class="queue-button-panel"><div id="btnFav" class="btn btn-primary"><i class="fa fa-star" aria-hidden="true"></i></div><div id="btnQueueClear" class="btn btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></div><div id="btnShuffleQueue" class="btn btn-primary"><i class="fa fa-random" aria-hidden="true"></i></div><input class="quickAddInput" placeholder="Quick Add"><div class="btn btn-primary quickAddSubmit"><i class="fa fa-search" aria-hidden="true"></i></div></div></div></div></div>');
+
+        $('.seQueue').click(() => displayQueueModal());
+}
+
+const addToQueue = url => {
+    //var myQueue = JSON.parse(localStorage.getItem('myQueue'));
+
+    //myQueue.push([text, url]);
+
+    //localStorage.setItem("myQueue", JSON.stringify(myQueue));
+    //triggerNoticiation(`${text} successfully added to your queue!`);
+}
+
+const displayQueueModal = () => {
+    //loadQueueItems();
+    //$('#btnFav').click(e => { loadFavoritesList() });
+    $('.queue-modal').css('display', 'flex');
+    $('body').toggleClass('noscroll');
+}
+
+const closeQueueModal = () => {
+    $('.queue-modal').hide();
+    $('.queue-table').html('<tbody><tr class="queue-table-headers"><th>Order</th><th>Name</th></tr></tbody>');
+    $('body').toggleClass('noscroll');
 }
