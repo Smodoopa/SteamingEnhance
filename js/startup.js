0d776cb1cb79519a9330bb2fdf858ed15f91df49
Smodoopa <smodoopa@gmail.com>
2023-06-30 19:12:27 -0400

    Queue 32

--- js/startup.js
+++ js/startup.js
@@ -54,7 +54,7 @@ const queueInit = () => {
 const loadQueue = () => {
     if (!localStorage.getItem('myQueue')) localStorage.setItem('myQueue', '[]');
 }
-<i class="bi bi-x-lg"></i>
+
 const loadQueueModal = () => {
         //Add Queue Modal to Site Nav Header
         $('body').prepend('<div class="queue-modal"><div class="queue-modal-content"><div class="queue-details"><div class="server-header"><h3 class="header-text">My Queue</h3><div id="close-btn" class="server-header-btn"><i class="bi bi-x-lg"></i></div></div><table class="queue-table"><tbody><tr class="queue-table-headers"><th>Order</th><th>Name</th></tr></tbody></table><div class="queue-button-panel"><div id="btnFav" class="btn btn-primary"><i class="fa fa-star" aria-hidden="true"></i></div><div id="btnQueueClear" class="btn btn-primary"><i class="fa fa-trash" aria-hidden="true"></i></div><div id="btnShuffleQueue" class="btn btn-primary"><i class="fa fa-random" aria-hidden="true"></i></div><input class="quickAddInput" placeholder="Quick Add"><div class="btn btn-primary quickAddSubmit"><i class="fa fa-search" aria-hidden="true"></i></div></div></div></div></div>');
